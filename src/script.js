a1669099558b64c56641a26b783e1455ec2170c2
karoliny bispo <karol-bobio@hotmail.com>
2024-10-10 20:20:29 -0300

    exemplo de uso de variavel, console, constante e insersao no readme

---
+++ src/script.js
@@ -0,0 +1,12 @@
+    //input
+    console.log("escreva seu nome"); //dentro das aspas eh o que deve ser mostrado na tela de saida
+                                //console eh para manipular o que aparece no console
+    
+    let nickname = "Gerogia"; // um espaco com nome nickname vai armazenar um valor
+                            //let declara variavel
+    //output
+    console.log("Bem vinda " + nickname);
+
+     const notificacao = " Pokemon diz que voce tem um novo "//constante nao muda
+
+     console.log(notificacao + "pokemon")
