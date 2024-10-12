ddaa4b6be74f7c165dce5db2cf099e66b6ae99fc
karoliny bispo <karol-bobio@hotmail.com>
2024-10-12 10:52:54 -0300

    alterecao no readme + add arq vetores e um arq de pratica

---
+++ src/pratica1.js
@@ -0,0 +1,13 @@
+    let poteCafe = "cafe pilao";
+    let poteAcucar = "Acucar criatal";
+    let poteBiscoto = "biscoito maizena";
+
+    console.log( "na cozinha tem : " + poteAcucar + " - " + poteCafe + " - " + poteBiscoto + " - "  );
+
+
+    // se eu quiser repetir a mensagem, coloque ela em uma variavel
+    const mensagem = "na conzinha tem: ";
+    console.log( mensagem + poteAcucar + " - " + poteCafe + " - " + poteBiscoto + " - "  );
+
+    
+    //So coloca a declarativa de uma variavel uma vez, no caso de poteAcucar, se eu desejar mudar o valor, so pego a variavel e mudo seu valor nao preciso novamente declarar let
---
+++ src/vetores.js
@@ -0,0 +1,13 @@
+
+    //Guardando varios valores em uma variavel. Isso significa que quero guardar uma colecao ou vetores.
+
+    let pokemon = ["pikachu", "charmadener", "bulbassor"];
+    //mostra todos os valores dentro da "colecao" pokemon
+    console.log(pokemon);
+    //mostrando em valor escifico do vetor
+    console.log(pokemon[1]);
+    //Ao colocar o nome da variavel e colocar ponto aparecem caixinhas roxas que sao funcoes/metodos (fazem alguma acao) que posso utilizar com essa variavel
+    //pokemon.
+    pokemon.pop()
+    // o pop exclui o ultimo elemento dos vetores
+    console.log(pokemon); 
